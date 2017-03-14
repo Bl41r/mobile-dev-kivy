10e05d32cfb935fd11f32cadb8c886a068ff3192
bl41r <dbsmith.dbs83@gmail.com>
2017-03-14 00:53:00 -0700

    login columns made

--- first-app/main.py
+++ first-app/main.py
@@ -2,7 +2,30 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
 kivy.require("1.8.0")
+
+
+class LoginScreen(GridLayout):
+    """Login screen."""
+
+    def __init__(self, **kwargs):
+        """Init."""
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="Username:"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+
+        self.add_widget(Label(text="Password:"))
+        self.password = TextInput(multiline=False, password=True)
+        self.add_widget(self.password)
+
+        self.add_widget(Label(text="Two Factor Auth:"))
+        self.tfa = TextInput(multiline=False)
+        self.add_widget(self.tfa)
 
 
 class SimpleKivy(App):
@@ -10,7 +33,7 @@ class SimpleKivy(App):
 
     def build(self):
         """Build our app."""
-        return Label(text="Hello World!")
+        return LoginScreen()
 
 if __name__ == "__main__":
     SimpleKivy().run()
